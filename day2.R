4031ebd18a672ccba9b5b8ac781bb0ca6a72e589
Musaddiqueahmed Dange <65895601+MusaddiqueahmedDange@users.noreply.github.com>
2020-09-23 20:32:28 +0530

    Add files via upload

---
+++ day2.R
@@ -0,0 +1,21 @@
+x<-1
+x
+x<-list(name=c('a','b','c','d'),age=c(22,21,22,21),marks=c(88,89,88,87),charecter=c('good','staisfactory','good','satisfactory'))
+x
+x<-list(name=c('a','b','c','d'),age=c(22,21,22,21),marks=c(88,89,88,87),charecter=c('good','staisfactory','good','satisfactory'))
+x
+ahmed<-data.frame(name=c('a','b','c','d'),age=c(22,21,22,21),marks=c(88,89,88,87),charecter=c('good','staisfactory','good','satisfactory'))
+ahmed
+x <- c(0:10, 50)
+x
+xm <- mean(x)
+xm
+c(xm, mean(x, trim = 0.10))
+d<-c(2,3,4,5,6)
+d
+dm <-mean(d)
+dm
+median(1:4)
+median(1,5,6,8,9,7,5,6,8,9,4,5,2,1,3,66,4,8,9)
+a<-c(1,2,3,3,4)
+mode(a)
