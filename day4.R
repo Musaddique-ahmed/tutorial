c4265ee1c42df771c5ede8728fcb633f90e85dae
Musaddiqueahmed Dange <65895601+MusaddiqueahmedDange@users.noreply.github.com>
2020-09-25 21:03:55 +0530

    Add files via upload

---
+++ day4.R
@@ -0,0 +1,32 @@
+﻿install.packages("dplyr")
+library(dplyr)
+library(nycflights13)
+data(flights)
+head(flights)
+flights<-data.frame(flights)
+#overview of the data
+glimpse(flights)
+#Select required rows and columns
+# dplyr approach
+select(flights, dep_time, arr_time, flight)
+#filter: Keep rows matching criteria
+# note: you can use comma or ampersand to represent AND condition
+filter(flights, month==1, day==1,origin=="EWR")
+# use pipe for OR condition
+filter(flights, carrier=="AA" | carrier=="UA")
+
+#“Chaining” or “Pipelining”
+# chaining method
+flights %>%
+  select(carrier, dep_delay) %>%
+  filter(dep_delay > 60)
+
+#Select carrier and DepDelay columns and sort by DepDelay
+flights %>%
+  select(carrier, dep_delay) %>%
+  arrange(dep_delay)
+#mutate: Add new variables
+#Create new variables that are functions of existing variables
+# store the new variable
+flights <- flights %>% mutate(Speed = distance/air_time*60)
+
